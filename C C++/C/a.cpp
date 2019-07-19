0bbd44fd13f9363b9b9c680e37b2a3e4b6186639
Jinbo <jinboom@foxmail.com>
2019-07-19 23:34:40 +0800

    准备添加C语言笔记

---
+++ C C++/C/a.cpp
@@ -0,0 +1,6 @@
+# include "stdio.h"
+int main(void)
+{
+	printf("vim");
+	return 0
+}
---
+++ C C++/C/hello.cpp
@@ -0,0 +1,18 @@
+/* helloworld.cpp */
+//#include <iostream>
+/*
+int main(int argc,char *argv[])
+{
+    std::cout << "hello, world" << std::endl;
+    return(0);
+}
+*/
+# include <stdio.h>
+int main(void)
+{
+    int a = 2;
+    int b = a + 2;
+    printf("hello world");
+    printf("%d",b);
+    return 0;
+}
