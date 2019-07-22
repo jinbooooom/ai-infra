8c428df351b38b836b0aea746f2eff4e573516ba
Jinbo <jinboom@foxmail.com>
2019-07-22 22:22:28 +0800

    添加指针等

--- C&C++/C/example/a.cpp
+++ C&C++/C/example/a.cpp
@@ -2,9 +2,17 @@
 int main()
 {
 	
-	int score[2][4] = {{2},{2,4}};
+	//int score[2][4] = {{2},{2,4}};
+	int num[] = {1,2,3};
+	printf("num,%d", *(num+2));
+	int a = 3;
+	int b = 3;
+	printf("%d",a++);
+	printf("%d",++b);
+/*
 	for(int i=0;i<2;++i)
 		for(int j=0;j<4;++j)
 			printf("%d ",score[i][j]);
+*/
 	return 0;
 }
